3fd794e8d8964b53190bb73c0f5f3ca6398e606d
Joselyn Armendariz <jossearmendariz19@gmail.com>
2024-11-29 03:27:45 -0500

    Examen Primer Parcial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,22 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
   password: string = '';
   confirmPassword: string = '';
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
   message: string = '';
   isMatch: boolean = false;
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
   validatePasswords() {
-    if (this.password === this.confirmPassword) {
-      this.message = 'Las contraseñas coinciden.';
-      this.isMatch = true;
-    } else {
-      this.message = 'Las contraseñas no coinciden.';
-      this.isMatch = false;
-    }
+    this.isMatch = this.password === this.confirmPassword;
+    this.message = this.isMatch
+      ? '¡Las contraseñas coinciden!'
+      : 'Las contraseñas no coinciden.';
   }
 }
